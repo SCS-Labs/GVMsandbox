b3c4bf6803b456c788c2b8800602732d8e535c79
Austin Songer <a.songer@protonmail.com>
2021-02-21 11:02:14 -0600

    Push the base

---
+++ modules/docker/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ueo pipefail
+
+# For /Build
+docker build
+
+#For /Base
+docker build
+
+#For /Openvas
+docker build
+
+
+# for /GVM
+docker build
---
+++ modules/docker/push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ueo pipefail
+
+
+docker push SCS-labs/gvm
+
+
+
